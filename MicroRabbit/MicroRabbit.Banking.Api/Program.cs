8f5c6ccbb1b82d7090d9bceed0730f01d964c59a
NguyenTienSang <tiensang07@gmail.com>
2024-11-16 15:53:45 +0700

    Partially completed Banking Microservice

--- MicroRabbit/MicroRabbit.Banking.Api/Program.cs
+++ MicroRabbit/MicroRabbit.Banking.Api/Program.cs
@@ -1,11 +1,36 @@
+using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Infra.IoC;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
+
 // Add services to the container.
+builder.Services.AddDbContext<BankingDbContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("BankingDbConnection"), sqlOptions =>
+             {
+                 sqlOptions.EnableRetryOnFailure(); // Automatically retry failed connections
+             }));
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Banking Microservice", Version = "v1" });
+});
+
+builder.Services.AddMediatR(configuration =>
+{
+    configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+DependencyContainer.RegisterServices(builder.Services);
 
 var app = builder.Build();
 
@@ -13,7 +38,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Banking Microservice V1");
+    });
 }
 
 app.UseHttpsRedirection();
